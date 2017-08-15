6b82605e6242112c3d6b9498d20c81d239b9794a
shazi7804 <shazi7804@gmail.com>
2017-08-15 13:35:41 +0800

    beaker don't downgrade low version

--- spec/acceptance/install_spec.rb
+++ spec/acceptance/install_spec.rb
@@ -27,19 +27,3 @@ describe 'upgrade puppet 5 corp104_puppet_agent' do
     end
   end
 end
-
-describe 'downgrade puppet 4 corp104_puppet_agent' do
-  context 'default parameters' do
-    it 'should install package' do
-      pp = <<-EOS
-        class { 'corp104_puppet_agent':
-          puppet_version => '1.10.6',
-        }
-      EOS
-
-      # Run it twice and test for idempotency
-      apply_manifest(pp, :catch_failures => true)
-      apply_manifest(pp, :catch_changes => true)
-    end
-  end
-end
